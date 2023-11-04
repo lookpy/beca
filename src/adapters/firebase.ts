8aa2307fd9d7a7444ded0bed9df5471a7423f9e5
Manoel Rocha <manoelmelo367@gmail.com>
2023-11-03 23:14:55 -0300

    test notification browser 3

--- src/adapters/firebase.ts
+++ src/adapters/firebase.ts
@@ -2,7 +2,7 @@ import { initializeApp  } from 'firebase-admin/app';
 import admin from 'firebase-admin';
 import { getMessaging } from 'firebase-admin/messaging';
 
-const serviceAccount = require("../private/spy-fake-firebase-adminsdk-85jji-49aafe4a9e.json")
+const serviceAccount = require("./spy-fake-firebase-adminsdk-85jji-49aafe4a9e.json")
 
 export const firebaseApp = initializeApp({
   credential: admin.credential.cert(serviceAccount),
